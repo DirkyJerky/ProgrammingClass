c75457bcce24ee5d1d00c838fd366342438a5dc9
Dirky Jerky <dirkyjerky@me.com>
2014-03-07 06:07:14 -1200

    Work on 92
    TODO Test the Drawer class

--- src/me/yoerger/geoff/edu/progClass/assignments/nine/nineTwo.java
+++ src/me/yoerger/geoff/edu/progClass/assignments/nine/nineTwo.java
@@ -1,13 +1,37 @@
 package me.yoerger.geoff.edu.progClass.assignments.nine;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+
 import me.yoerger.geoff.edu.progClass.assignments.Analysis;
 import me.yoerger.geoff.edu.progClass.assignments.Printer;
+import me.yoerger.geoff.edu.progClass.bookClasses.Picture;
+import me.yoerger.geoff.edu.progClass.mod9.Drawer;
 
 public class nineTwo implements Analysis{
 
-	public static void main(String[] args) {
-		System.out.println("Tell Geoff he needs to actually do something here");
+	public static void main(String[] args) throws Exception {
+		Picture canvas = new Picture(500, 500);
+		canvas.setAllPixelsToAColor(Color.WHITE);
+		drawTest(canvas);
+		canvas.show();
+		Thread.sleep(10000);
+		System.exit(0);
 	}
+	public static void drawTest(Picture pictre) {
+		Drawer drawer = new Drawer(pictre);
+		drawer.set(Color.BLACK, 10);
+		drawer.drawOval(100, 100, 50, 75);
+		
+		drawer.set(Color.BLUE, 3);
+		drawer.fillOval(100, 100, 50, 75);
+		// Do it again and again and again....
+		drawer.set(Color.CYAN, 5);
+		drawer.drawHollow(new Rectangle(new Point(10, 10), new Dimension(20,  40)));
+	}
+	
 	@Override
 	public void printQuestions(Printer printer) {
 		printer.print("TODO");
---
+++ src/me/yoerger/geoff/edu/progClass/mod9/Drawer.java
@@ -0,0 +1,79 @@
+package me.yoerger.geoff.edu.progClass.mod9;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+import me.yoerger.geoff.edu.progClass.bookClasses.Picture;
+
+public class Drawer {
+	private Picture thisPicture;
+	private Graphics2D graphics;
+	
+	public Drawer(Picture canvas) {
+		this.thisPicture = canvas;
+		this.graphics = (Graphics2D) this.thisPicture.getGraphics();
+	}
+	
+	public void drawHollow(Shape shape) {
+		this.draw(shape, false);
+	}
+	public void draw(Shape shape) {
+		this.draw(shape, true);
+	}
+	public void draw(Shape shape, boolean hollow) {
+		this.draw(shape, Color.BLACK, hollow);
+	}
+	public void draw(Shape shape, Color color, boolean hollow) {
+		Color curr = this.graphics.getColor();
+		this.setColor(color);
+		this.graphics.draw(shape);
+		if(!hollow) this.graphics.fill(shape);
+		this.setColor(curr);
+	}
+	public void drawLine(Color color, int thickness, Point2D first, Point2D second) {
+		Stroke oldStroke = this.graphics.getStroke();
+		this.graphics.setStroke(new BasicStroke(thickness));
+		this.draw(new Line2D.Float(first, second), color, false);
+		this.graphics.setStroke(oldStroke);
+	}
+	public void set(Color color, float lineWidth) {
+		this.setColor(color);
+		this.setLineWidth(lineWidth);
+	}
+	public float setLineWidth(float len) {
+		BasicStroke oldStroke = (BasicStroke) this.graphics.getStroke();
+		this.graphics.setStroke(new BasicStroke(len));
+		return oldStroke.getLineWidth();
+	}
+	// START REQ
+	public void setColor(Color color) {
+		this.graphics.setColor(color);
+	}
+	public void drawString(String string, int xPos, int yPos) {
+		this.thisPicture.drawString(string, xPos, yPos);
+	}
+	public void  drawRect(Rectangle2D rect) {
+		this.draw(rect);
+	} 
+	public void  fillRect(Rectangle2D rect) {
+		this.graphics.fill(rect);
+	}
+	public void  drawOval(int x, int y, int width, int height) {
+		this.graphics.drawOval(x, y, width, height);
+	} 
+	public void  fillOval(int x, int y, int width, int height) {
+		this.graphics.fillOval(x, y, width, height);
+	} 
+	public void  drawArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
+		this.graphics.drawArc(x, y, width, height, startAngle, arcAngle);
+	} 
+	public void  fillArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
+		this.graphics.fillArc(x, y, width, height, startAngle, arcAngle);
+	} 
+}
