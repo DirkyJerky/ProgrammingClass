b017cd7156e0805326e49845714b7d43b2e53210
DirkyJerky <dirkyjerky@me.com>
2014-03-18 09:15:01 -0500

    Fix some simple errors

--- src/me/yoerger/geoff/edu/progClass/assignments/nine/nineFour.java
+++ src/me/yoerger/geoff/edu/progClass/assignments/nine/nineFour.java
@@ -1,20 +1,38 @@
 package me.yoerger.geoff.edu.progClass.assignments.nine;
 
-import java.awt.Graphics2D;
+import java.awt.Graphics;
 
 import me.yoerger.geoff.edu.progClass.assignments.Analysis;
 import me.yoerger.geoff.edu.progClass.assignments.Printer;
 import me.yoerger.geoff.edu.progClass.bookClasses.Picture;
-import me.yoerger.geoff.edu.progClass.mod9.Scaler;
+
+import org.imgscalr.Scalr;
+
+import sun.awt.image.ImageFormatException;
 
 public class nineFour implements Analysis {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		Picture kTile = new Picture(200, 200);
-		Graphics2D graphics = (Graphics2D) kTile.getGraphics();
-		Scaler scaler = new Scaler(graphics);
-		scaler.apply(200, 200);
-		kTile.show();
+		String fileLoc = "P:\\Programming\\Workspace\\ProgrammingClass\\etc\\Flowers.jpeg";
+		// String fileLoc = FileChooser.pickAFile();
+		Picture kTile;
+		if (fileLoc == null) {
+			kTile = new Picture(200, 200);
+		} else {
+			try {
+				kTile = new Picture(fileLoc);
+			} catch (ImageFormatException e) {
+				kTile = new Picture(200, 200);
+			}
+		}
+		Picture scaledPic = new Picture(Scalr.resize(kTile.getBufferedImage(), 250));
+		// TODO: Find out how to mirror the image about a diagonal
+		Picture diagonalMirror = new Picture(250, 250);
+		// TODO: Create a canvas and mirror the first image to all sides
+		Picture canvas = new Picture(500, 500);
+		Graphics graphics = canvas.getGraphics();
+
 	}
 
 	@Override
--- src/me/yoerger/geoff/edu/progClass/mod9/Scaler.java
+++ src/me/yoerger/geoff/edu/progClass/mod9/Scaler.java
@@ -1,22 +1,29 @@
 package me.yoerger.geoff.edu.progClass.mod9;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 
 import me.yoerger.geoff.edu.progClass.bookClasses.Picture;
 
+@Deprecated
+// Use the imgscalr lib instead!
 public class Scaler {
-	private final Graphics2D graphics;
-
-	public Scaler(Graphics graphics) {
-		this.graphics = (Graphics2D) graphics;
-	}
+	private final BufferedImage bi;
 
 	public Scaler(Picture picture) {
-		this.graphics = picture.createGraphics();
+		this(picture.getBufferedImage());
 	}
 
-	public Graphics apply(int w, int h) {
-		return this.graphics; // TODO
+	public Scaler(BufferedImage bi) {
+		this.bi = bi;
+	}
+
+	public BufferedImage apply(int w, int h) {
+		AffineTransform AffT = AffineTransform.getScaleInstance(w / this.bi.getWidth(),
+		        h / this.bi.getHeight());
+		AffineTransformOp AffTop = new AffineTransformOp(AffT,
+		        AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+		return AffTop.filter(this.bi, null);
 	}
 }
