ed33abdb844d94f2f568ab3cd6d8146aae151cb7
Dirky Jerky <geoffreyiy1@gmail.com>
2014-03-31 22:09:02 -0500

    Add another option to Main.java

--- src/me/yoerger/geoff/edu/progClass/main/Main.java
+++ src/me/yoerger/geoff/edu/progClass/main/Main.java
@@ -6,15 +6,24 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
-		System.out.println("Do you want to:\n"
-				+ "1: Run an assignment or \n"
-				+ "2: Show the required analysis questions for an assignment?");
+		System.out
+		        .println("Do you want to:\n"
+		                + "1: Run an assignment or \n"
+		                + "2: Show the required analysis questions for an assignment or \n"
+		                + "3: Do both?");
 		String runOrPrint = in.next();
-		if (runOrPrint.toLowerCase().matches("r.*|p.*|1.*") || runOrPrint.trim().length() == 0) { // See if they entered 1, ques, ?, analysis, etc.
+		if (runOrPrint.toLowerCase().matches("r.*|p.*|1.*")) {
+			// See if they entered 1, ques, ?, analysis, etc.
 			MainRun.main(args);
-		} else if (runOrPrint.toLowerCase().matches("q.*|2.*|a.*|\\?.*")) { // See if they entered 2, program, run, r, prog, etc.
-			MainPMR.main(args);
-		}
+		} else
+			if (runOrPrint.toLowerCase().matches("q.*|2.*|a.*|\\?.*")) {
+				// See if they entered 2, program, run, r, prog, etc.
+				MainPMR.main(args);
+			}
+			else {
+				MainRun.main(args);
+				MainPMR.main(args);
+			}
 		System.exit(0);
 	}
 }
--- src/me/yoerger/geoff/edu/progClass/main/MainRun.java
+++ src/me/yoerger/geoff/edu/progClass/main/MainRun.java
@@ -10,19 +10,22 @@ public class MainRun {
 		Class<?> mainClazz = AssesmentGetter.getAssignment(in);
 
 		System.out.print("\n\n\n\n\n\n\n\n\n\n"); // Ten new lines
-//		if(shouldRun) {
-			Method main = mainClazz.getMethod("main", String[].class);
-			main.invoke(null, new Object[] { ourArgs }); // Invoke main
-			Thread.sleep(10000); // Wait 10 seconds
-//		} else {
-//			try {
-//				Printer printer = new Printer();
-//				Method printQuestions = mainClazz.getMethod("printQuestions", Printer.class);
-//				printQuestions.invoke(mainClazz.newInstance(), new Object[] { printer });
-//			} catch (NoSuchMethodException nsme) {
-//				System.out.println("No analysis questions present in class " + mainClazz.getName());
-//			}
-//		}
+		// if(shouldRun) {
+		Method main = mainClazz.getMethod("main", String[].class);
+		main.invoke(null, new Object[] { ourArgs }); // Invoke main
+		Thread.sleep(5000); // Wait 5 seconds
+		// } else {
+		// try {
+		// Printer printer = new Printer();
+		// Method printQuestions = mainClazz.getMethod("printQuestions",
+		// Printer.class);
+		// printQuestions.invoke(mainClazz.newInstance(), new Object[] { printer
+		// });
+		// } catch (NoSuchMethodException nsme) {
+		// System.out.println("No analysis questions present in class " +
+		// mainClazz.getName());
+		// }
+		// }
 		System.exit(0); // Terminate and cleanup
 	}
 
