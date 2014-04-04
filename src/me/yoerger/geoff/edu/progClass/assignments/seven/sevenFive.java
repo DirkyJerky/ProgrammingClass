b6a473b73aa63f9485b7f632a10453903693c778
Geoff Yoerger <yoerggeo000@ecsdnet.org>
2014-04-04 18:42:50 -0500

    Fix the RandomColor class

--- src/me/yoerger/geoff/edu/progClass/assignments/seven/sevenFive.java
+++ src/me/yoerger/geoff/edu/progClass/assignments/seven/sevenFive.java
@@ -14,63 +14,64 @@ import me.yoerger.geoff.edu.progClass.mod7.RandomColor;
 import com.google.common.base.Preconditions;
 
 /**
- * Assignment 7.5
- * Make a simple animation in a box
+ * Assignment 7.5 Make a simple animation in a box
+ * 
  * @see RandomColor
  */
-public class sevenFive implements Analysis{
+public class sevenFive implements Analysis {
 	public static void main(String[] args) throws Exception {
-		
+
 		final int BOX_SIZE = 200;
 		final int ANIM_SCALE = 5;
 		final int TIMEOUT = 10;
-		
+
 		final int BORDER_LINE_WIDTH = 3;
 		final int ANIM_LINE_WIDTH = 10;
-		
+
 		final Color ANIM_COLOR = Color.RED;
 		final Color OUTLINE_COLOR = Color.GREEN;
-		
+
 		final boolean REVERSABLE = true;
-		
+
 		// Checks
 		Preconditions.checkArgument(TIMEOUT >= 5, "Timeout less than 5");
 		// The thread handler will hate you if you go less than 5.
 		// The poor turtles will to.
-		
+
 		// Feel free to play around with the constants
-		
+
 		final World world = new World();
 		final Turtle turtle = new Turtle(100, 300, world);
 		final Turtle animTurtle = new Turtle(100, 300, world);
 		final Random r = new Random(System.currentTimeMillis());
-		
+
 		turtle.hide();
 		animTurtle.hide(); // Hide both the turtles
-		turtle.faceUp(); 
+		turtle.faceUp();
 		animTurtle.faceUp(); // Align them up
 		turtle.setPenColor(OUTLINE_COLOR);
 		animTurtle.setPenColor(ANIM_COLOR);
 		turtle.setPenWidth(BORDER_LINE_WIDTH);
 		animTurtle.setPenWidth(ANIM_LINE_WIDTH);
-		
+
 		for (int i = 0; i < 4; i++) { // Draw the outline for the animation
 			turtle.forward(BOX_SIZE);
 			turtle.turnRight();
 		}
-		
+
 		final Vector<Integer> vector = new Vector<>();
 		for (int i = 0; i < BOX_SIZE; i += ANIM_SCALE) {
 			vector.add(i);
 		}
 		System.out.println("Close the window to stop the animation.");
 		Runnable anim = new Runnable() {
+			@Override
 			public void run() {
-				if(REVERSABLE && r.nextBoolean()) {
+				if (REVERSABLE && r.nextBoolean()) {
 					Collections.reverse(vector); // Spice it up a little bit
 				}
 				animTurtle.setPenColor(RandomColor.make());
-				for(int point : vector) {
+				for (int point : vector) {
 					animTurtle.getPen().clearPath();
 					animTurtle.setPenDown(false);
 					animTurtle.goTo(100, 300 - point);
@@ -81,11 +82,11 @@ public class sevenFive implements Analysis{
 					animTurtle.goTo(100, 300 - point);
 					try {
 						Thread.sleep(TIMEOUT);
-					} catch (InterruptedException e) {} //Ignore 
+					} catch (InterruptedException e) {} // Ignore
 				}
 			}
 		};
-		while(world.isShowing()) {
+		while (world.isShowing()) {
 			anim.run();
 		}
 		System.exit(0);
--- src/me/yoerger/geoff/edu/progClass/mod7/RandomColor.java
+++ src/me/yoerger/geoff/edu/progClass/mod7/RandomColor.java
@@ -2,30 +2,31 @@ package me.yoerger.geoff.edu.progClass.mod7;
 
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 
 /**
  * Used to get a random color of the rainbow
+ * 
  * @author yoerggeo000
  * @since Module 7
  */
 public class RandomColor {
 
-	private final static ArrayList<Color> colors = new ArrayList<>();
-	/**
-	 * @return A random color of the rainbow
-	 */
-	public static Color make() {
-		colors.clear();
+	private static ArrayList<Color> colors = new ArrayList<>();
+	private static Random random = new Random();
+	static {
 		colors.add(Color.RED);
 		colors.add(Color.GREEN);
 		colors.add(Color.BLUE);
 		colors.add(Color.YELLOW);
 		colors.add(new Color(138, 43, 226)); // Purple
 		colors.add(Color.ORANGE);
-		System.out.println(colors.size());
-		Collections.shuffle(colors);
-		return colors.get(1);
+	}
+
+	/**
+	 * @return A random color of the rainbow
+	 */
+	public static Color make() {
+		return colors.get(random.nextInt(colors.size()));
 	}
 }
