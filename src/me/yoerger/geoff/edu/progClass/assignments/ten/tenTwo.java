29ccf15b02f2b7629b952cbe50e59cbb8034df4f
DirkyJerky <dirkyjerky@me.com>
2014-04-08 15:18:24 -0500

    Test 10.2

--- src/me/yoerger/geoff/edu/progClass/assignments/ten/tenTwo.java
+++ src/me/yoerger/geoff/edu/progClass/assignments/ten/tenTwo.java
@@ -2,6 +2,8 @@ package me.yoerger.geoff.edu.progClass.assignments.ten;
 
 import me.yoerger.geoff.edu.progClass.assignments.Analysis;
 import me.yoerger.geoff.edu.progClass.assignments.Printer;
+import me.yoerger.geoff.edu.progClass.bookClasses.MidiPlayer;
+import me.yoerger.geoff.edu.progClass.mod7.Range;
 
 /**
  * Assignment 10.2
@@ -10,7 +12,15 @@ import me.yoerger.geoff.edu.progClass.assignments.Printer;
 public class tenTwo implements Analysis{
 
 	public static void main(String[] args) {
-		System.out.println("Tell Geoff he needs to actually do something here");
+		MidiPlayer player1 = new MidiPlayer();
+	    player1.setInstrument(MidiPlayer.PIANO);
+	    
+	    int[] note = {60,60,67,67,69,69};
+	    int[] duration = {250,250,250,250,250,250};     
+	      
+	    for(int i : new Range(-1, 5).range()) {
+	    	player1.playNote(note[i], duration[i]);
+	    }
 	}
 
 	@Override
