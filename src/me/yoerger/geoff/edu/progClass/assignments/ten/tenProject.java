3f4ac1ab91f9f25c9bb55ad5a7c4417febfd33ad
Dirky Jerky <dirkyjerky@me.com>
2014-05-01 13:39:51 -0500

    Work on some testing

--- src/me/yoerger/geoff/edu/progClass/assignments/ten/tenProject.java
+++ src/me/yoerger/geoff/edu/progClass/assignments/ten/tenProject.java
@@ -1,8 +1,12 @@
 package me.yoerger.geoff.edu.progClass.assignments.ten;
 
+import java.io.IOException;
+
 import junit.framework.TestCase;
 import me.yoerger.geoff.edu.progClass.assignments.Analysis;
 import me.yoerger.geoff.edu.progClass.assignments.Printer;
+import me.yoerger.geoff.edu.progClass.main.AssesmentGetter;
+import me.yoerger.geoff.edu.progClass.mod10.FileHandler;
 
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
@@ -35,8 +39,48 @@ public class tenProject extends TestCase implements Analysis{
 //	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testForAssignments() {
+		for(int i = 6; i <= 10; i++) {
+			for(int j = 1; j <= 7; j++ ) {
+				try {
+					assertNotNull("Assesment Getter returns assignment: " + i + " " + j, AssesmentGetter.getAssignment(i, j));
+				} catch (ClassNotFoundException e) {
+					fail("Assignment " +  i + " " + j + " does not exist formally");
+				}
+			}
+		}
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testAssesmentGetterArgs01() throws ClassNotFoundException {
+		AssesmentGetter.getAssignment(5, 1);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testAssesmentGetterArgs10() throws ClassNotFoundException {
+		AssesmentGetter.getAssignment(6, 0);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testAssesmentGetterArgs00() throws ClassNotFoundException {
+		AssesmentGetter.getAssignment(11, 1);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testAssesmentGetterArgs() throws ClassNotFoundException {
+		AssesmentGetter.getAssignment(6, 8);
+	}
+	
+	@Test
+	public void testAssignmentsConditions() throws ClassNotFoundException, NoSuchMethodException, SecurityException {
+		for(int i = 6; i <= 10; i++) {
+			for(int j = 1; j <= 7; j++ ) {
+				Class<?> assignment = AssesmentGetter.getAssignment(i, j);
+				assignment.getMethod("main", String[].class);
+				assignment.getMethod("printQuestions", Printer.class);
+			}
+		}
+	}
+	
+	@Test(expected = IOException.class) 
+	public void testFileHandlerWNull() throws IOException {
+		new FileHandler(null).readFile();
 	}
 	
 	
